08609114a9663f5069304a647457ba27b1b96368
Kim.Se-Yeon <tpdussla93@gmail.com>
2022-05-19 15:02:11 +0900

    Add configuring after opening device with termios struct

--- pm5000s.h
+++ pm5000s.h
@@ -38,6 +38,30 @@ public:
     }
     ~SerialPort() { Close(); }
 
+    constexpr static speed_t BAUD_RATE = B9600;
+
+    bool Open(const std::string& device_path) {
+        if (IsOpened()) {
+            Close();
+        }
+
+        device_path_ = device_path;
+
+        Open();
+
+        return IsOpened();
+    }
+    bool Open(std::string&& device_path) {
+        if (IsOpened()) {
+            Close();
+        }
+        device_path_ = std::move(device_path);
+
+        Open();
+
+        return IsOpened();
+    }
+
     bool IsOpened() const { return fd_ >= 0; }
 
     // return true if success
@@ -51,15 +75,77 @@ public:
                      "Failed to close serial device " + device_path_);
             return false;
         }
+        fd_ = -1;
+        device_path_.clear();
 
         return true;
     }
+
+    const std::string& GetDevicePath() const { return device_path_; }
 
 private:
     void Open() {
         fd_ = open(device_path_.c_str(), O_RDWR);
         if (!IsOpened()) {
             LogError(errno, "open", "Failed to open device " + device_path_);
+
+            fd_ = -1;
+            device_path_.clear();
+
+            return;
+        }
+
+        termios tty;
+
+        if (tcgetattr(fd_, &tty) != 0) {
+            LogError(errno, "tcgetattr");
+            Close();
+            return;
+        }
+
+        // these configurations are guided by
+        // https://blog.mbedded.ninja/programming/operating-systems/linux/linux-serial-ports-using-c-cpp/
+
+        // Control Modes
+        tty.c_cflag &= ~CSTOPB;   // one stop bit
+        tty.c_cflag &= ~PARENB;   // disable praity
+        tty.c_cflag &= ~CSIZE;    // Clear all the size bits
+        tty.c_cflag |= CS8;       // 8bits per byte
+        tty.c_cflag &= ~CRTSCTS;  // disable RTS/CTS hardware flow control
+        tty.c_cflag |= CREAD | CLOCAL;
+
+        // Local Modes
+        tty.c_lflag &= ~ICANON;  // non-canonical mode
+        // Disable echo just in case
+        tty.c_lflag &= ~ECHO;    // Disable echo
+        tty.c_lflag &= ~ECHOE;   // Disable erasure
+        tty.c_lflag &= ~ECHONL;  // Disable new-line echo
+        tty.c_lflag &= ~ISIG;    // Disable signal chars(INTR, QUIT, SUSP)
+
+        // Input Modes
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY);  // Turn off s/w flow ctrl
+        tty.c_iflag &=
+            ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR |
+              ICRNL);  // Disable any special handling of received bytes
+
+        // Output Modes
+        tty.c_oflag &= ~OPOST;  // Prevent special interpretation of output
+                                // bytes (e.g. newline chars)
+        tty.c_oflag &= ~ONLCR;  // Prevent conversion of newline to carriage
+                                // return/line feed
+
+        tty.c_cc[VTIME] = 10;  // Wait for up to 1s(10 deciseconds)
+        tty.c_cc[VMIN] = 2;  // Wait until getting 2 characters at least, which
+                             // is (HEAD, LEN)
+
+        // Set in/out baud rate
+        cfsetispeed(&tty, BAUD_RATE);
+        cfsetospeed(&tty, BAUD_RATE);
+
+        if (tcsetattr(fd_, TCSANOW, &tty) != 0) {
+            LogError(errno, "tcsetattr");
+
+            Close();
             return;
         }
     }
